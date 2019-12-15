f10750ebcf326041b3df7a66fdd67140f276aa12
yuxiang <1780768307@qq.com>
2019-12-15 17:18:57 +0800

    get

--- js/index.js
+++ js/index.js
@@ -3,11 +3,9 @@ import jQuery from 'jquery'
 console.log(jQuery)
 console.log(jQuery('div'))
 
-// const axios = require('axios');
 import {$, $$, myAjax} from './tools'
 
 let deleteBtn
-
 const getList = () => {
   myAjax({
     url: 'http://localhost:3000/todo',
@@ -19,7 +17,7 @@ const getList = () => {
       const {id, title} = item
       $('.list .list-group').innerHTML += `
       <li class="list-group-item">
-        <a href="show.html?objectId=${id}">${title}</a>
+        <a href=show.html?objectId=${id}">${title}</a>
         <i data-id="${id}" class="fa fa-close"></i>
       </li>
       `
@@ -41,17 +39,27 @@ const getList = () => {
 }
 
 getList()
+let content = document.querySelector('.form-control')
 const todoInput = $('.form-todo input[type=text]')
 $('#create-todo').addEventListener('click', function () {
-  myAjax({
-    url: 'http://localhost:3000/todo',
-    method: 'POST',
-    data: {
-      title: todoInput.value
-    }
-  }).then(() => {
-    todoInput.value = ''
-    getList()
-  }).catch(e => console.log(e))
-
+    // console.log(content.value)
+  if (content.value !== '') {
+    myAjax({
+      url: 'http://localhost:3000/todo',
+      method: 'POST',
+      data: {
+        title: todoInput.value
+      }
+    }).then(() => {
+      todoInput.value = ''
+      getList()
+    }).catch(e => console.log(e))
+  } else {
+    alert('便签名不能为空')
+  }
 })
+
+
+
+
+
